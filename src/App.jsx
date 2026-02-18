6f1afdb4332e771fd28de55f0f4140511a5a2c64
gokulan011 <gokulharish@034.com>
2026-02-18 15:40:06 +0530

    hii

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import './cssfiles/Navbar.css'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import './Style.css'
+import './cssfiles/Navbar.css'
 import './cssfiles/Productcart.css'
 import './cssfiles/Home.css'
 import './cssfiles/Men.css'
@@ -11,14 +11,14 @@ import './cssfiles/Women.css'
 import './cssfiles/Kids.css'
 import './cssfiles/Login.css'
 import './cssfiles/Searchpage.css'
-import Register from './navpages/Register'
 import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
-import Men from './navpages/Men'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Register from './navpages/Register'
 import Women from './navpages/Women'
 import Kids from './navpages/kids'
+import Men from './navpages/Men'
 
 
 function App() {
